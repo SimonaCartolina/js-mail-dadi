3a9192c9c0dfe56db8965c1861c16f36f4810421
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-18 11:41:28 +0200

    updated js

--- myscript.js
+++ myscript.js
@@ -48,38 +48,34 @@
 
         const emailList=['simonacartolina@hotmail.com', 'gino@gmail.it', 'ginetta@gmail.it' ];
         const userEmail= prompt ('Insert your email');
-        const found=true;
 
 
         const ulElement=document.querySelector('ul');
         const login= document.querySelector ('#Login');
 
-        
+        let found=true;
 
 
         login.addEventListener('click', function(){
 
+
         for(i=0; i < emailList.length ; i++){
-            const liElement=document.createElement('li');
+            const li=document.createElement('li');
             console.log(i, emailList[i]);
 
             /*3. stampa un messaggio appropriato sull’esito del controllo.*/
             if(emailList[i] === userEmail){
-
                 found=true;
-
+                console.log('Logged in');
+                
                 document.querySelector ('ul').textContent="Logged in";
-                ulElement.append(liElement);
+                ulElement.appendChild(li);
             }
             else{
+                console.log('Access denied');
                 document.querySelector ('ul').textContent="Access denied";
-                ulElement.append(liElement);
+                            ulElement.appendChild(li);
             }
 }})
 
-if(found=true) {
-    console.log('Logged in');
-}
-else if(found=false){
-    console.log('Logged in');
-}
+
