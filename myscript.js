41bf0fab14817f099470aa3b3a9bec2d4a2b5dfc
Simona Cartolina <simonacartolina@hotmail.com>
2023-05-18 11:31:46 +0200

    corrected exercise of the email

--- myscript.js
+++ myscript.js
@@ -46,39 +46,40 @@
 
         /*second exercise*/
 
-        /*1.Chiedi all’utente la sua email*/ 
-
+        const emailList=['simonacartolina@hotmail.com', 'gino@gmail.it', 'ginetta@gmail.it' ];
         const userEmail= prompt ('Insert your email');
-        console.log(userEmail);
+        const found=true;
 
-        /*4.controlla che sia nella lista di chi può accedere*/
-        const listEmail=['simonacartolina@hotmail.com', 'gino@gmail.it', 'ginetta@gmail.it' ];
-        console.log(listEmail);
 
         const ulElement=document.querySelector('ul');
         const login= document.querySelector ('#Login');
 
+        
+
 
         login.addEventListener('click', function(){
 
-        for(i=0; i===listEmail; i++){
+        for(i=0; i < emailList.length ; i++){
             const liElement=document.createElement('li');
+            console.log(i, emailList[i]);
 
             /*3. stampa un messaggio appropriato sull’esito del controllo.*/
-            if( listEmail[i] === userEmail){
-                console.log('Logged in');
-                document.querySelector ('#Login').textContent="Logged in";
+            if(emailList[i] === userEmail){
+
+                found=true;
+
+                document.querySelector ('ul').textContent="Logged in";
                 ulElement.append(liElement);
             }
             else{
-                console.log('Wrong Email');
-                document.querySelector ('#Login').textContent="Wrong Email";
+                document.querySelector ('ul').textContent="Access denied";
                 ulElement.append(liElement);
-
             }
-        }
-    }
-        )
+}})
 
-
-
+if(found=true) {
+    console.log('Logged in');
+}
+else if(found=false){
+    console.log('Logged in');
+}
